67fc2ce99aed790fa21613c919f4005939bdaf76
Charles Qian <charleshqian@gmail.com>
2021-08-09 15:08:08 -0700

    completed MVP

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,35 @@
-export const ADD_ONE = "ADD_ONE";
-
+//export const ADD_ONE = "ADD_ONE";
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = 'CLEAR_DISPLAY'
+export const M_PLUS = 'M_PLUS'
+export const M_R = 'M_R'
+export const M_C = 'M_C'
 
-export const addOne = () => {
-    return({type:ADD_ONE});
-}
+// export const addOne = () => {
+//     return({type:ADD_ONE});
+// }
 
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
+}
+
+export const applyOp = (op) => {
+    return({type:CHANGE_OPERATION, payload:op});
+}
+
+export const applyClear = () => {
+    return ({type:CLEAR_DISPLAY})
+}
+
+export const applyMP = () => {
+    return ({type:M_PLUS})
+}
+
+export const applyMR = () => {
+    return ({type:M_R})
+}
+
+export const applyMC = () => {
+    return ({type:M_C})
 }
